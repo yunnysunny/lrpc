cc043796114fa3182eafdc2a702b572831946014
yunnysunny <yunnysunny@gmail.com>
2018-02-22 21:39:38 +0800

    增加文档

--- demo/basic.js
+++ demo/basic.js
@@ -1,15 +1,5 @@
-# lrpc
-项目源码摘自淘宝开源项目 [pandora](https://github.com/midwayjs/pandora) ，pandora 是一个淘宝开源的进程管理工具，里面进程间通信的模块很具有参考意义，所以将其单独分离出来做成一个项目。
-
-## 安装
-
-npm install lrpc --save
-
-## 使用示例
-
-```javascript
 const assert = require('assert');
-const {Server,Client,util} = require('lrpc');
+const {Server,Client,util} = require('../index');
 const msg = {
     name: 'midway-messenger',
 };
@@ -35,7 +25,3 @@ new Promise(function(resolve) {
 }).catch(function() {
     assert(false);
 });
-```
-
-## 在线文档
-
